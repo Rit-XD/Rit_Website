90f476fe5ed8c47b395f90b330a4e81b2351bf4e
Senne Christiaens <senne.christiaens@icloud.com>
2024-03-07 16:46:38 +0100

    splash page

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,49 +2,20 @@ import '@/styles/blocks.scss'
 import '@/styles/globals.scss'
 
 import {cms} from '@/cms'
-import {Footer} from '@/components/footer/Footer'
 import {FooterSchema} from '@/components/footer/Footer.schema'
-import {Header} from '@/components/header/Header'
 import {HeaderSchema} from '@/components/header/Header.schema'
 import {Locale, defaultLocale, locales} from '@/utils/locale'
 import localFont from 'next/font/local'
 
-// const metropolis = localFont({
-//   src: [
-//     {
-//       path: '../../assets/fonts/Metropolis-Regular.woff2',
-//       weight: '400',
-//       style: 'normal'
-//     },
-//     {
-//       path: '../../assets/fonts/Metropolis-Medium.woff2',
-//       weight: '500',
-//       style: 'normal'
-//     },
-//     {
-//       path: '../../assets/fonts/Metropolis-SemiBold.woff2',
-//       weight: '600',
-//       style: 'normal'
-//     }
-//   ]
-// })
-
-// const sourceSerifPro = localFont({
-//   variable: '--sourceSerifPro',
-//   display: 'swap',
-//   src: [
-//     {
-//       path: '../../assets/fonts/source-serif-pro-v7-latin-regular.woff2',
-//       weight: '400',
-//       style: 'normal'
-//     },
-//     {
-//       path: '../../assets/fonts/source-serif-pro-v7-latin-700.woff2',
-//       weight: '700',
-//       style: 'normal'
-//     }
-//   ]
-// })
+const inter = localFont({
+  src: [
+    {
+      path: '../../assets/fonts/Inter.woff2',
+      weight: '100 900',
+      style: 'normal'
+    }
+  ]
+})
 
 export async function generateStaticParams(): Promise<{locale: Locale}[]> {
   return locales.map(locale => ({locale}))
@@ -65,8 +36,7 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <body>
-        {/* className={`${metropolis.className} ${sourceSerifPro.variable}`} */}
+      <body className={`${inter.className}`}>
         {/* <Header data={header} language={params.locale} /> */}
         {children}
         {/* <Footer data={footer} language={params.locale} /> */}
--- src/blocks/splash/Splash.tsx
+++ src/blocks/splash/Splash.tsx
@@ -1,5 +1,4 @@
 import {Button} from '@/ui/Button'
-import {Title} from '@/ui/Title'
 import {fromModule} from '@/utils/styler/Styler'
 import {Infer} from 'alinea'
 import css from './Splash.module.scss'
@@ -12,19 +11,21 @@ type SplashProps = Infer<typeof SplashSchema>
 export function Splash({block}: {block: SplashProps}) {
   const {title, text, button_left, button_right} = block
   return (
-    <div>
-      {title && <Title.H1>{title}</Title.H1>}
+    <div className={styles.splash()}>
+      {title && <h1>{title}</h1>}
       {text && <p>{text}</p>}
-      {button_left && (
-        <Button href={button_left?.url || '/'}>
-          {button_left?.label || button_left?.title || 'Zorgcentra'}
-        </Button>
-      )}
-      {button_right && (
-        <Button href={button_right?.url || '/'}>
-          {button_right?.label || button_right?.title || 'Vrijwilligers'}
-        </Button>
-      )}
+      <div className={styles.splash.buttons()}>
+        {button_left && (
+          <Button href={button_left?.url || '/'}>
+            {button_left?.label || button_left?.title || 'Zorgcentra'}
+          </Button>
+        )}
+        {button_right && (
+          <Button href={button_right?.url || '/'}>
+            {button_right?.label || button_right?.title || 'Vrijwilligers'}
+          </Button>
+        )}
+      </div>
     </div>
   )
 }
