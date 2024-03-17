62f0d98a9744bde992d553a8e3a98e966c3ea4a4
Senne Christiaens <senne.christiaens@icloud.com>
2024-03-17 13:38:32 +0100

    add partners block

--- src/blocks/partners/Partners.tsx
+++ src/blocks/partners/Partners.tsx
@@ -1,11 +1,8 @@
-import {Block} from '@/blocks/Block'
+import css from '@/blocks/partners/Partners.module.scss'
 import {Container} from '@/ui/Container'
 import {Image} from '@/ui/Image'
-import {Title} from '@/ui/Title'
 import {fromModule} from '@/utils/styler/Styler'
 import {Infer} from 'alinea'
-import Link from 'next/link'
-import css from './Partners.module.scss'
 import {PartnerSchema} from './Partners.schema'
 
 const styles = fromModule(css)
@@ -13,66 +10,37 @@ const styles = fromModule(css)
 type PartnersData = Infer<typeof PartnerSchema>
 
 export function Partners({block}: {block: PartnersData}) {
-  const {title, subtitle_left, subtitle_right, partners} = block
+  const {title, image, partners} = block
 
   return (
-    <Block className={styles.partners()}>
-      <Container>
-        <div className={styles.partners.content()}>
-          <Title.H4>{title}</Title.H4>
-          <div className={styles.partners.content.flex()}>
-            <div className={styles.partners.content.left()}>
-              <h5 className={styles.partners.content.subtitle()}>
-                {subtitle_left}
-              </h5>
-              <ul className={styles.partners.content.list()}>
-                {partners
-                  .filter(partner => partner?.position === 'left')
-                  .map((partner, index) => (
-                    <li key={index}>
-                      <Link
-                        className={styles.partners.content.image()}
-                        href={partner.link.url}
-                      >
-                        {partner.image?.src && (
-                          // eslint-disable-next-line jsx-a11y/alt-text
-                          <Image {...partner.image} sizes="120px" />
-                        )}
-                        {partner.title && <p>{partner.title}</p>}
-                      </Link>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-            <div className={styles.partners.content.right()}>
-              <h5 className={styles.partners.content.subtitle()}>
-                {subtitle_right}
-              </h5>
-              <ul className={styles.partners.content.list()}>
-                {partners
-                  .filter(partner => partner?.position === 'right')
-                  .map((partner, index) => (
-                    <li key={index}>
-                      <Link
-                        className={styles.partners.content.image()}
-                        href={partner.link.url}
-                      >
-                        {partner?.image?.src && (
-                          <Image
-                            {...partner.image}
-                            alt={partner.title}
-                            sizes="120px"
-                          />
-                        )}
-                        {partner.title && <p>{partner.title}</p>}
-                      </Link>
-                    </li>
-                  ))}
-              </ul>
-            </div>
+    <Container className={styles.background()}>
+      <h2 className={styles.title()}>{title}</h2>
+      <div className={styles.partners()}>
+        <ul className={styles.partners.list()}>
+          {partners.map((partner, index) => (
+            <li key={index} className={styles.partners.item()}>
+              {partner.icon?.src && (
+                <div className={styles.partners.item.icon()}>
+                  <Image width={64} height={56} src={partner.icon.src} />
+                </div>
+              )}
+              <p className={styles.partners.item.description()}>
+                {partner.description}
+              </p>
+            </li>
+          ))}
+        </ul>
+        {image?.src && (
+          <div className={styles.partners.image()}>
+            <Image
+              width={769}
+              height={576}
+              sizes="(max-width: 1024px) 100vw, 50vw"
+              src={image.src}
+            />
           </div>
-        </div>
-      </Container>
-    </Block>
+        )}
+      </div>
+    </Container>
   )
 }
